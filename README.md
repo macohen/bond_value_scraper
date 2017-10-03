2241fa7d18e9cb20b134203f29aac52e8600d783
Mark Cohen <mark.cohen@viacom.com>
2017-10-02 21:24:28 -0400

    added command line args.

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ Then, you just need to click save and copy and paste the table back into a sprea
 ## Run
 1. Use Python 3.2+
 1. pip install -r requirements.txt
+1. Install [geckodriver](https://github.com/mozilla/geckodriver) on your PATH for Firefox support
 1. copy your bonds.csv file to the root. don't add it to git, please. I don't want to know
-1. python bond_scraper.py
+1. python bond_scraper.py --value_as_of=&lt;month and year you want to calculate the value for&gt; --bond_csv=&lt;the file with the list of bonds to calculate&gt;
+    * --value_as_of=11/2017
+    * --bond_csv=sample_bonds.csv (see included sample_bonds.csv for reference format) 
+1. Once the script is done, you should be able to go to the Firefox instance that Selenium opened and save the results as a file
--- bond_scraper.py
+++ bond_scraper.py
@@ -3,9 +3,10 @@ import csv
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import argparse
 
 def format_date(issue_date):
-    issue_date_as_date = datetime.strptime(issue_date, "%b-%y")
+    issue_date_as_date = datetime.strptime(issue_date, "%m/01/%Y")
     return issue_date_as_date.strftime("%m/%y").strip()
 
 
@@ -18,31 +19,54 @@ def format_currency(denom):
 Create a CSV with the headers "SerialNumber", "IssueDate", "Denom"
 """
 def run(bond_file, redemption_date):
-    with open(bond_file, 'r', encoding='utf-8') as bonds_csv:
+    with open(bond_file, 'r', encoding='utf-8-sig') as bonds_csv:
         bonds_reader = csv.DictReader(bonds_csv)
-        browser = webdriver.Firefox()
-        browser.get("https://www.treasurydirect.gov/BC/SBCPrice")
-        browser.implicitly_wait(3)
+        driver = webdriver.Firefox()
+        driver.get("https://www.treasurydirect.gov/BC/SBCPrice")
+        driver.implicitly_wait(30)
 
-        for idx, row in enumerate(bonds_reader):
+        if redemption_date != None:
+            send_redemption_date(driver, redemption_date)
+
+        for row in bonds_reader:
             serial_number = row["SerialNumber"]
             issue_date = row["IssueDate"]
             denom = row["Denom"]
-            print(row)
 
             if(len(serial_number.strip()) > 0):
-                # does the $ need to be replaced by %?
-                browser.find_element_by_name("SBCForm")
-                browser.find_element_by_name("SerialNumber").clear()
-                browser.find_element_by_name("SerialNumber").send_keys(serial_number)
-                browser.find_element_by_name("IssueDate").clear()
-                browser.find_element_by_name("IssueDate").send_keys(format_date(issue_date))
-                browser.find_element_by_name("RedemptionDate").clear()
-                browser.find_element_by_name("RedemptionDate").send_keys(redemption_date)
-                Select(browser.find_element_by_name("Denomination")).select_by_value(format_currency(denom))
-                browser.find_element_by_name("btnAdd.x").click()
+                try:
+                    update_denomination(denom, driver)
+                    send_serial_number(driver, serial_number)
+                    send_issuedate(driver, issue_date)
+                    add_bond(driver)
+                except ValueError as e:
+                    print("Error {1} for value: {0}".format(row, e))
+
+def add_bond(driver):
+    driver.find_element_by_name("btnAdd.x").click()
+
+def send_issuedate(driver, issue_date):
+    driver.find_element_by_name("IssueDate").clear()
+    driver.find_element_by_name("IssueDate").send_keys(format_date(issue_date))
+
+
+def send_serial_number(driver, serial_number):
+    driver.find_element_by_name("SerialNumber").clear()
+    driver.find_element_by_name("SerialNumber").send_keys(serial_number)
+
+def update_denomination(denom, driver):
+    denom_elem = Select(driver.find_element_by_name("Denomination"))
+    denom_elem.select_by_value(format_currency(denom))
+
+def send_redemption_date(driver, redemption_date):
+    driver.find_element_by_name("RedemptionDate").clear()
+    driver.find_element_by_name("RedemptionDate").send_keys(redemption_date)
 
 if __name__ == '__main__':
-    bond_file = "bonds.csv"
-    redemption_date = "11/2016"
-    run(bond_file, redemption_date)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--value_as_of', type=str, dest='value_as_of')
+    parser.add_argument('--bond_csv', type=str, dest='bond_csv')
+    args = parser.parse_args()
+    bond_file = args.bond_csv
+    value_as_of = args.value_as_of
+    run(bond_file, value_as_of)
